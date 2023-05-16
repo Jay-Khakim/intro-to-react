7b1a204ac1017fa20e6f9b9f671003c8456724ab
Jay-Khakim <mgmediajay@gmail.com>
2023-05-16 17:05:04 +0900

    Prectice with callback and state management

--- src/PracticeActivity.js
+++ src/PracticeActivity.js
@@ -1,17 +1,43 @@
+import { useState } from "react";
+
 function Parent() {
+    const colours = ["red", "green", "orange", "yellow", "blue"]
+    const [textColor, setColour] = useState("black");
+    const [colourIndex, setColourIndex] = useState(0);
+
+    const changeTextColour =()=>{
+        const nextColourIndex = (colourIndex + 1 ) % colours.length
+        setColourIndex(nextColourIndex)
+        setColour(colours[nextColourIndex])
+    }
     return(
         <div>
             <h1>Hello from the parent</h1>
-            <Child greeting = {"Hello wordl"}/>
+            <Child1 callback={changeTextColour}/>
+            <Child2 newColour = {textColor}/>
         </div>
     )
 }
 
-function Child(props){
+function Child1(props){
+    const handleClick = ()=>{
+        props.callback()
+    }
+    return( 
+        <div>
+            <button onClick={handleClick}>
+                Change the text color
+            </button>
+        </div>
+    )
+}
+function Child2(props){
     return(
-        <p>
-            {props.greeting}
-        </p>
+        <div>
+            <p style={{color: props.newColour}}>
+            This text changes a color
+            </p>
+        </div>
     )
 }
 
