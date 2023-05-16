89377d2ef9ca4d0ef17cfb589ce0503bf1f42e45
Jay-Khakim <mgmediajay@gmail.com>
2023-05-16 16:03:12 +0900

    custom imput component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<div>Hello world!</div>
+			<CustomInput />
+			<CustomInput />
+		</div>
+	);
+}
+
+function CustomInput() {
+	const [value, setValue] = useState("");
+	const handleChange = event => {
+		setValue(event.target.value);
+	};
+	return <input type="text" value={value} onChange={handleChange} />;
 }
 
 export default App;
