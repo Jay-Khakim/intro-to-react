1899eae20d0a5814308c7154964785e98e3a8a81
Jay-Khakim <mgmediajay@gmail.com>
2023-05-18 13:54:38 +0900

    cUMULATIVE TASK

---
+++ src/App2.js
@@ -0,0 +1,69 @@
+import {useState} from 'react'
+
+function App(){
+    const [items, setItems] = useState([])
+    const onAddItem = (item) => {
+        setItems([...items, item])
+    }
+
+    const removeItem = (index) =>{
+        const newItems = [...items]
+        newItems.splice(index, 1)
+        setItems(newItems)
+    }
+
+    return(
+        <div>
+            <Header onAddItem={onAddItem}/>
+            <List items={items} removeItem={removeItem} />
+
+        </div>
+    )
+}
+
+
+function Header(props){
+    const [itemText, setItemText] = useState('')
+    const handleChange = (event) =>{
+        setItemText(event.target.value)
+    }
+    const handleSubmit = (event)=>{
+        event.preventDefault()
+        props.onAddItem(itemText)
+        setItemText('')
+    }
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input 
+                type='text'
+                placeholder='Enter Item'
+                value={itemText}
+                onChange={handleChange}
+                />
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+
+function List(props){
+    const handleRemoveItem = (index)=>{
+        props.removeItem(index)
+    }
+    return(
+        <div>
+            <ul>
+                {props.items.map((item, index)=>(
+                    <li key={index}>
+                        {item}
+                        <button onClick={(index)=>handleRemoveItem(index)}>Remove</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default App
